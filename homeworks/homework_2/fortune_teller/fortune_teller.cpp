5f65b9cf20e9b0cc055dd60eb7f87916d41d12a9
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2022-09-04 21:19:21 +0200

    Rename homeworks

--- homeworks/homework_2/fortune_teller/fortune_teller.cpp
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -6,7 +6,7 @@
 namespace {
 static const std::map<std::string, std::string> kNoun{
     {"spring", "STL guru"},
-    {"summer", "C++ maniac"},
+    {"summer", "C++ expert"},
     {"autumn", "coding beast"},
     {"winter", "software design hero"},
 };
@@ -19,11 +19,11 @@ static const std::array<std::string, 3UL> kEnding{
 
 // This homework only makes sense before we learn if statements and for loops.
 int main() {
-  std::cout << "Welcome to the name annotator program!" << std::endl;
+  std::cout << "Welcome to the fortune teller program!" << std::endl;
   std::cout << "Please enter your name:" << std::endl;
   std::string name{};
   std::cin >> name;
-  std::cout << "Please enter time of year when you were born:" << std::endl;
+  std::cout << "Please enter the time of year when you were born:" << std::endl;
   std::cout << "(pick from 'spring', 'summer', 'autumn', 'winter')"
             << std::endl;
   std::string time_of_year{};
@@ -35,12 +35,12 @@ int main() {
   std::cout << "Please enter an adjective:" << std::endl;
   std::cin >> adjective;
   adjectives.push_back(adjective);
-  std::cout << "Please enter an adjective:" << std::endl;
+  std::cout << "Please enter another adjective:" << std::endl;
   std::cin >> adjective;
   adjectives.push_back(adjective);
 
   std::cout << std::endl;
-  // Igor is a ruthless STL guru that eats UB for breakfast
+  //  Igor is a ruthless STL guru that eats UB for breakfast
   std::cout << "Here is your description:" << std::endl;
   std::cout << name << ", the " << adjectives[name.size() % adjectives.size()]
             << " " << kNoun.at(time_of_year) << " that "
