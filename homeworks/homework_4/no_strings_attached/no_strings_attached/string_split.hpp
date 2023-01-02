0abf2d27864a6c2b7eca0215b38bec1ef46973e5
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2023-01-02 13:48:42 +0100

    Move code around

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_split.hpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_split.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <limits>
 #include <string>
 #include <vector>
 
@@ -7,20 +8,24 @@ namespace no_strings_attached {
 
 inline std::vector<std::string> Split(const std::string& str,
                                       const std::string& delimeter) {
+  return Split(str, delimeter, std::numeric_limits<int>::max());
+}
+
+inline std::vector<std::string> Split(const std::string& str,
+                                      const std::string& delimeter,
+                                      int max_chunks) {
   std::vector<std::string> result;
   auto start = 0U;
   auto end = str.find(delimeter);
+  int chunk_count{};
   while (end != std::string::npos) {
+    if (++chunk_count > max_count) { return result; }
     result.emplace_back(str.substr(start, end - start));
     start = end + delimeter.length();
     end = str.find(delimeter, start);
   }
   result.emplace_back(str.substr(start, end));
   return result;
-}
-
-inline std::vector<std::string> Split(const std::string& str, char delimeter) {
-  return Split(str, std::string{delimeter});
 }
 
 }  // namespace no_strings_attached
